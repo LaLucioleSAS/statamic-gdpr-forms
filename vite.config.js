58752858c1634cafd7d10bc7bdbe0d52fabaa58a
Theo Renouf <theorenouf56@gmail.com>
2026-06-11 13:44:35 +0200

    Correction vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/js/cp.js',
                 'resources/js/request.js',
             ],
             publicDirectory: 'resources/dist',
